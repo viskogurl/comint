dd66e15b732b0e5c0a691eda3d27188753c2b8cf
viskogurl <54771783+viskogurl@users.noreply.github.com>
2023-08-28 13:41:30 -0500

    3 files modified, 1 deleted

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('Hello!');
+function minifySVGPaths() {
+  const input = document.getElementById('input');
+  const output = document.getElementById('output');
+  let minifiedHTML = input.value;
+
+  const svgRegex =
+    /<svg.*?>[\s\S]*?<\/svg>|<!-- svg path(s) excluded for brevity -->/gi;
+  const svgs = input.value.match(svgRegex);
+
+  if (svgs) {
+    svgs.forEach((svg) => {
+      if (svg.includes('<path')) {
+        minifiedHTML = minifiedHTML.replace(
+          svg,
+          '<!-- svg path(s) excluded for brevity -->'
+        );
+      } else {
+        const pathComment = '<!-- svg path(s) excluded for brevity -->';
+        minifiedHTML = minifiedHTML.replace(svg, pathComment);
+      }
+    });
+  }
+  output.textContent = minifiedHTML;
+}
